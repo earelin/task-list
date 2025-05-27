05621f6ccc1fa9220614712e6a698592bbbe36e9
Xavier Carriba <xavier.carriba@gmail.com>
2025-05-27 03:17:59 +0200

    Run MongoDB in a Compute Engine instance

--- infrastructure/gcloud/components/backend.tf
+++
@@ -1,6 +0,0 @@
-terraform {
-  backend "gcs" {
-    bucket  = "task-list-tf-state-bucket"
-    prefix  = "terraform/state"
-  }
-}
---
+++ infrastructure/gcloud/components/modules/mongodb/variables.tf
@@ -0,0 +1,19 @@
+variable "gcp_project_id" {
+  type    = string
+}
+
+variable "gcp_region" {
+  type    = string
+}
+
+variable "mongodb_user_name_secret_name" {
+  type    = string
+}
+
+variable "mongodb_user_password_secret_name" {
+  type    = string
+}
+
+variable "mongodb_version" {
+  type    = string
+}
