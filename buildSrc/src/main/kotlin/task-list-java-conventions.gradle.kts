682ddbbf595f67827c4bd47fb398fc0d9ca298da
Xavier Carriba <xavier.carriba@gmail.com>
2025-05-28 23:30:35 +0200

    Upgrade to Java 21

--- buildSrc/src/main/kotlin/task-list-java-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/task-list-java-conventions.gradle.kts
@@ -25,11 +25,11 @@ spotbugs {
 
 java {
     toolchain {
-        languageVersion = JavaLanguageVersion.of(17)
+        languageVersion = JavaLanguageVersion.of(21)
 
     }
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
 
 dependencies {
