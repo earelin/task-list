76424f34b905e5e99ba99f633f9df367b9b466bf
Xavier Carriba <xavier.carriba@gmail.com>
2025-08-18 16:27:09 +0200

    Fix permissions

--- infrastructure/gcloud/components/github_actions/main.tf
+++ infrastructure/gcloud/components/github_actions/main.tf
@@ -5,10 +5,10 @@ resource "google_service_account" "github_actions" {
 }
 
 resource "google_artifact_registry_repository_iam_member" "task_list_repository_github_actions_writer" {
-  count      = google_service_account.github_actions.email == null ? 0 : 1
   project    = var.gcp_project_id
   location   = var.gcp_region
   repository = var.task_list_repository_name
-  role       = "roles/artifactregistry.writer"
+  role       = "roles/artifactregistry.createOnPushWriter"
   member     = "serviceAccount:${google_service_account.github_actions.email}"
 }
+
