a1a6301d7b3bb8ac13c3f3c0279f584b71820185
Xavier Carriba <xavier.carriba@gmail.com>
2025-08-18 15:56:27 +0200

    Moving registry to shared resources

--- infrastructure/gcloud/environments/shared/main.tf
+++ infrastructure/gcloud/environments/shared/main.tf
@@ -1,6 +1,19 @@
+resource "google_artifact_registry_repository" "task_list_repository" {
+  location      = local.gcp_region
+  repository_id = "task-list"
+  description   = "Task List Application Docker Repository"
+  format        = "DOCKER"
+
+  docker_config {
+    immutable_tags = true
+  }
+}
+
 module "github_actions" {
   source             = "../../components/github_actions"
   gcp_project_id     = local.gcp_project_id
+  gcp_region         = local.gcp_region
+  task_list_repository_name = google_artifact_registry_repository.task_list_repository.name
 }
 
 module "network" {
