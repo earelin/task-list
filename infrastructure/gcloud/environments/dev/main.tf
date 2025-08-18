adafeb486c0690d85e596e99fb7101e02c0b9ca7
Xavier Carriba <xavier.carriba@gmail.com>
2025-08-19 01:17:36 +0200

    Continous Integration Pipeline (#4)

--- infrastructure/gcloud/environments/dev/main.tf
+++ infrastructure/gcloud/environments/dev/main.tf
@@ -1,9 +1,9 @@
 module "app" {
-  source                              = "../../components/app"
-  app_version                         = "1.0.0"
-  gcp_project_id                      = local.gcp_project_id
-  gcp_region                          = local.gcp_region
-  gcp_subnetwork_name                 = var.shared_subnetwork_name
+  source                               = "../../components/app"
+  app_version                          = "1.0.0"
+  gcp_project_id                       = local.gcp_project_id
+  gcp_region                           = local.gcp_region
+  gcp_subnetwork_name                  = var.shared_subnetwork_name
   github_actions_service_account_email = var.github_actions_service_account_email
 }
 
